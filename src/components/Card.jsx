9dc10647fcc2dbc164e78c91134fbb737d4c8d53
Arno Boenders <arno.boen@student.arteveldehs.be>
2024-06-09 20:15:34 +0200

    💄Styling card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ image, title, link }) {
       <img className="w-full" src={image} alt={title} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
-        <a href={link} className="text-blue-500 hover:text-blue-700">
+        <a href={link} className="text-orange-300 hover:underline">
           Meer
         </a>
       </div>
