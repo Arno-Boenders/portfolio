982709be3ff91a243bf121a977f7d76dabe6a6ac
Arno Boenders <arno.boen@student.arteveldehs.be>
2024-06-16 12:24:55 +0200

    💄Styling navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,10 +4,16 @@ import { ROUTES } from "../routes/routes";
 
 export default function Navigation() {
   return (
-    <nav>
-      <NavLink to={ROUTES.home.path}>{ROUTES.home.title}</NavLink>
-      <a href={ROUTES.work.path}>{ROUTES.work.title}</a>
-      <a href={ROUTES.about.path}>{ROUTES.about.title}</a>
+    <nav className="sticky top-0 flex justify-end items-center gap-8 pr-5 text-2xl h-20 z-10 backdrop-blur-sm bg-white/1">
+      <NavLink className="hover:text-orange-300" to={ROUTES.home.path}>
+        {ROUTES.home.title}
+      </NavLink>
+      <a className="hover:text-orange-300" href={ROUTES.work.path}>
+        {ROUTES.work.title}
+      </a>
+      <a className="hover:text-orange-300" href={ROUTES.about.path}>
+        {ROUTES.about.title}
+      </a>
     </nav>
   );
 }
