9ec0550f7b1cdca1d51e8411bb3fcc93864f5182
Arno Boenders <arno.boen@student.arteveldehs.be>
2024-07-08 15:38:45 +0200

    🐛Fix for hover when small screen

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,15 @@ export default function Card({ image, title, link, technologies }) {
   return (
     <NavLink
       to={link}
-      className="w-80 h-60 rounded-xl overflow-hidden hover:shadow-lg hover:scale-105 duration-300 relative group"
+      className="w-80 h-60 rounded-xl overflow-hidden lg:hover:shadow-lg lg:hover:scale-105 duration-300 relative group"
     >
       <div className="w-full h-full relative">
         <img className="w-full h-full object-cover" src={image} alt={title} />
-        <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-70 duration-300 flex flex-col justify-center items-center text-white lg:text-center">
-          <div className="opacity-0 group-hover:opacity-100 duration-300 font-bold text-2xl">
+        <div className="absolute inset-0 bg-black bg-opacity-0 lg:group-hover:bg-opacity-70 duration-300 flex flex-col justify-center items-center text-white lg:text-center">
+          <div className="opacity-0 lg:group-hover:opacity-100 duration-300 font-bold text-2xl">
             {title}
           </div>
-          <p className="opacity-0 group-hover:opacity-100 duration-300">
+          <p className="opacity-0 lg:group-hover:opacity-100 duration-300">
             {technologies}
           </p>
         </div>
